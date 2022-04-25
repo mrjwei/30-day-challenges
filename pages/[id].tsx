61df3e996b2723e935d5a30a8e92ca1618cd9660
Jesse Wei <business.jessecreatives@gmail.com>
2022-04-25 14:54:27 +0900

    style: adjust h4 style in markdowns

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -15,7 +15,7 @@ const Challenge = ({ids, activeId, content}: {ids: string, activeId: string, con
         dangerouslySetInnerHTML={{
           __html: `
             <div
-              class="prose max-w-none"
+              class="prose max-w-none prose-h4:text-lg prose-h4:underline"
             >
               ${content}
             </div>
